a068d4dda0b18acbe811dfa1ef1e91c56f6258dd
Libby Colbert <colbert.libby@gmail.com>
2018-10-20 20:00:22 -0400

    Add more details link

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -8,10 +8,13 @@ const ListItem = (props) => {
     const { spot } = props
     return (
         <div className="list-items">
+        {console.log(spot.canonicalUrl)}
             <div className="venue-photo">Photo goes here</div>
             {`${spot.location ? spot.location.formattedAddress[0] : `Address: Not found address`} 
                 ${spot.location ? spot.location.formattedAddress[1] : ``}`}
             <p>Rating: {spot.rating ? spot.rating : `rating : Unpublished rating`}</p>
+            {/* <a href="http://foursquare.com/v/4aed3d58f964a5205ecd21e3?ref=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}">More Details</a> */}
+            <a href={`${spot.canonicalUrl ? spot.canonicalUrl: ``}`}>More Details</a>
         </div>
     )
 }
