119d7aa6f5638863bc022551812121fe64d4fe48
Libby Colbert <colbert.libby@gmail.com>
2018-11-19 19:15:32 -0500

    Updates

---
+++ precache-manifest.e0586bdea336893b4bf0ca708f596bdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "367667708a752083cdbea691726cfac2",
+    "url": "/night-spot-app/static/media/powered-by-foursquare-grey.36766770.svg"
+  },
+  {
+    "revision": "a9bb7a23f1d86899161468c0f2dbcf93",
+    "url": "/night-spot-app/static/media/party2.a9bb7a23.jpg"
+  },
+  {
+    "revision": "421ba5be2f2b2abbeccd",
+    "url": "/night-spot-app/static/js/runtime~main.421ba5be.js"
+  },
+  {
+    "revision": "e22952b21617c633312d",
+    "url": "/night-spot-app/static/js/main.e22952b2.chunk.js"
+  },
+  {
+    "revision": "d4e9aa1ebf8c6d5b2b9e",
+    "url": "/night-spot-app/static/js/1.d4e9aa1e.chunk.js"
+  },
+  {
+    "revision": "e22952b21617c633312d",
+    "url": "/night-spot-app/static/css/main.9238caaf.chunk.css"
+  },
+  {
+    "revision": "7d7d6edd725de952754df85445d66eab",
+    "url": "/night-spot-app/index.html"
+  }
+];
